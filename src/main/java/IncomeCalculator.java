60895eeb4c344f4cfcc44c14de6835d9e6ecff32
Acer HOME <261700@student.pwr.edu.pl>
2021-10-17 23:09:23 +0200

    Initial commit

---
+++ src/main/java/IncomeCalculator.java
@@ -0,0 +1,20 @@
+public class IncomeCalculator {
+
+    private int totalMonthlyIncomeInPLN;
+    private int numOfFamilyMembers;
+
+    public IncomeCalculator(int totalMonthlyIncomeInPLN, int numOfFamilyMembers) {
+        this.totalMonthlyIncomeInPLN = totalMonthlyIncomeInPLN;
+        this.numOfFamilyMembers = numOfFamilyMembers;
+    }
+
+    public DecisionType incomePerFamilyMember(){
+        FinancialData financialData = new FinancialData(totalMonthlyIncomeInPLN,numOfFamilyMembers);
+
+        if( totalMonthlyIncomeInPLN/numOfFamilyMembers > 2000){
+            return DecisionType.POSITIVE;
+        }else{
+            return DecisionType.NEGATIVE;
+        }
+    }
+}
